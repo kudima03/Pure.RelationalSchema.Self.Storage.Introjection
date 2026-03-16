9753d96edea3cf11e9ef5ec1e5d5ee49475fc1e0
kudima03 <kudima03@gmail.com>
2026-03-16 20:29:39 +0300

    Replace hash lookup with UUID in TableFromRow

--- src/Pure.RelationalSchema.Self.Storage.Introjection/Internals/TableFromRow.cs
+++ src/Pure.RelationalSchema.Self.Storage.Introjection/Internals/TableFromRow.cs
@@ -1,4 +1,3 @@
-using Pure.HashCodes.Abstractions;
 using Pure.Primitives.Abstractions.String;
 using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Index;
@@ -6,7 +5,6 @@ using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.Self.Schema.Columns;
 using Pure.RelationalSchema.Self.Schema.Tables;
 using Pure.RelationalSchema.Storage.Abstractions;
-using Pure.RelationalSchema.Storage.HashCodes;
 
 namespace Pure.RelationalSchema.Self.Storage.Introjection.Internals;
 
@@ -16,24 +14,22 @@ internal sealed record TableFromRow : ITable
 
     private readonly IStoredSchemaDataSet _schemaDataset;
 
-    public TableFromRow(IDeterminedHash rowHash, IStoredSchemaDataSet schemaDataset)
+    public TableFromRow(IString tableId, IStoredSchemaDataSet schemaDataset)
     {
         _row = schemaDataset[new TablesTable()]
-            .Single(x => new RowHash(x).SequenceEqual(rowHash));
+            .Single(x => x.Cells[new UuidColumn()].Value.TextValue == tableId.TextValue);
         _schemaDataset = schemaDataset;
     }
 
     public IString Name => _row.Cells[new NameColumn()].Value;
 
     public IEnumerable<IColumn> Columns =>
-        new TableColumns(new RowHash(_row), _schemaDataset).Select(x => new ColumnFromRow(
-            x,
-            _schemaDataset
-        ));
+        new TableColumns(_row.Cells[new UuidColumn()].Value, _schemaDataset).Select(
+            x => new ColumnFromRow(x, _schemaDataset)
+        );
 
     public IEnumerable<IIndex> Indexes =>
-        new TableIndexes(new RowHash(_row), _schemaDataset).Select(x => new IndexFromRow(
-            x,
-            _schemaDataset
-        ));
+        new TableIndexes(_row.Cells[new UuidColumn()].Value, _schemaDataset).Select(
+            x => new IndexFromRow(x, _schemaDataset)
+        );
 }
